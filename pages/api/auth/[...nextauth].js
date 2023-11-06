6dc5a4d596c6e80a55889f0ca98bb51db77a8273
yangdi0710 <phanlengocduy07102000@gmail.com>
2023-11-06 22:46:38 +0700

    06/11 11PM

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,8 +6,8 @@ import GoogleProvider from "next-auth/providers/google";
 export default NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
   adapter: MongoDBAdapter(clientPromise),
--- pages/products/new.js
+++ pages/products/new.js
@@ -5,8 +5,13 @@ export default function NewProduct(){
         <Layout>
             <h1>New Product</h1>
             <div className="flex flex-col">
+                <label>Product Name</label>
                 <input type="text" placeholder="Product name"/>
+                <label>Description</label>
                 <textarea placeholder="Description"></textarea>
+                <label>Price (USD)</label>
+                <input type="text" placeholder="Price"/>
+                <button className="btn-primary">Save</button>
             </div>
         </Layout>
     )
