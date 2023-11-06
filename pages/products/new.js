cae3efa3ffd0fc294d46c3b188973adcd0473e35
yangdi0710 <phanlengocduy07102000@gmail.com>
2023-11-06 16:35:00 +0700

    06/11 4PM

--- pages/products/new.js
+++ pages/products/new.js
@@ -3,7 +3,7 @@ import Layout from "@/components/Layout"
 export default function NewProduct(){
     return (
         <Layout>
-            <h1 className="text-2xl text-blue-900 mb-10">New Product</h1>
+            <h1>New Product</h1>
             <div className="flex flex-col">
                 <input type="text" placeholder="Product name"/>
                 <textarea placeholder="Description"></textarea>
