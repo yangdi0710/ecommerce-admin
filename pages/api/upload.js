0cc89322aa188771b7d15f71007f45f5f24cbe48
yangdi0710 <phanlengocduy07102000@gmail.com>
2023-11-14 22:56:44 +0700

    11PM 14/11

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,5 +1,8 @@
 import multiparty from "multiparty";
-import { resolve } from "styled-jsx/css";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import fs from "fs";
+import mime from "mime-types"
+const bucketName = "yangdi0710-next-ecommerce";
 
 export default async function handle(req, res) {
   const form = new multiparty.Form();
@@ -9,9 +12,32 @@ export default async function handle(req, res) {
       resolve({ fields, files });
     });
   });
-  console.log('length:', files);
-  console.log(fields);
-  return res.json("OK");
+  console.log("length:", files.file.length);
+  const client = new S3Client({
+    region: "ap-southeast-1",
+    credentials: {
+      accessKeyId: process.env.S3_ACCESS_KEY,
+      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+    },
+  });
+  const links = []
+  for (const file of files.file) {
+    const ext = file.originalFilename.split(".").pop()
+    const newFilename = Date.now() + '.' + ext
+    console.log({ext, file})
+    await client.send(
+      new PutObjectCommand({
+        Bucket: bucketName,
+        Key: newFilename,
+        Body: fs.readFileSync(file.path),
+        ACL: 'public-read',
+        ContentType: mime.lookup(file.path)
+      })
+    );
+    const link = `https://${bucketName}.s3.amazonaws.com/${newFilename}`;
+    links.push(link)
+  }
+  return res.json({links});
 }
 
 export const config = {
