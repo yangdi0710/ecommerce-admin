0ed23e3ecc9dccc1d9b2a3ef98f0274388e6972a
yangdi0710 <phanlengocduy07102000@gmail.com>
2023-12-11 16:59:43 +0700

    11/12 5PM 5:56

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,16 @@
-import { useSession, signIn, signOut } from "next-auth/react";
+import { useSession, signIn } from "next-auth/react";
 import Nav from "@/components/Nav";
+import { useState } from "react";
 
 export default function Layout({ children }) {
+  const [showNav, setShowNav] = useState(false)
   const { data: session } = useSession();
   if (!session) {
     return (
       <div className="bg-blue-900 w-screen h-screen flex items-center">
         <div className="text-center w-full">
           <button
-            onClick={() => signIn('google')}
+            onClick={() => signIn("google")}
             className="bg-white p-2 px-4 rounded-md"
           >
             Login with Google
@@ -18,10 +20,28 @@ export default function Layout({ children }) {
     );
   }
   return (
-    <div className="bg-blue-700 min-h-screen flex">
-      <Nav />
-      <div className="bg-white flex-grow my-2 mr-2 rounded-lg p-4">
-        { children }
+    <div className="bg-gray-100 min-h-screen">
+      <button onClick={() => setShowNav(true)}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6 mt-2 ml-2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+      </button>
+      <div className="flex">
+        <Nav show={showNav} />
+        <div className="bg-white flex-grow rounded-lg p-4">
+          {children}
+        </div>
       </div>
     </div>
   );
