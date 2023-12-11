4fa2f79a339852d95027cddd621345ece6743813
yangdi0710 <phanlengocduy07102000@gmail.com>
2023-12-11 22:51:34 +0700

    11/12 11PM 6:15

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,10 @@
 import { useSession, signIn } from "next-auth/react";
 import Nav from "@/components/Nav";
 import { useState } from "react";
+import Logo from "./Logo";
 
 export default function Layout({ children }) {
-  const [showNav, setShowNav] = useState(false)
+  const [showNav, setShowNav] = useState(false);
   const { data: session } = useSession();
   if (!session) {
     return (
@@ -21,27 +22,30 @@ export default function Layout({ children }) {
   }
   return (
     <div className="bg-gray-100 min-h-screen">
-      <button onClick={() => setShowNav(true)}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="w-6 h-6 mt-2 ml-2"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-          />
-        </svg>
-      </button>
+      <div className="md:hidden flex items-center">
+        <button onClick={() => setShowNav(true)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6 m-2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        </button>
+        <div className="flex grow justify-center">
+          <Logo />
+        </div>
+      </div>
       <div className="flex">
         <Nav show={showNav} />
-        <div className="bg-white flex-grow rounded-lg p-4">
-          {children}
-        </div>
+        <div className="bg-white flex-grow rounded-lg p-4">{children}</div>
       </div>
     </div>
   );
--- pages/api/products.js
+++ pages/api/products.js
@@ -1,5 +1,6 @@
 import { Product } from "@/models/Product";
 import { mongooseConnect } from "@/lib/mongoose";
+import { isAdminRequest } from "./auth/[...nextauth]";
 
 export default async function handle(req, res) {
   const { method } = req;
