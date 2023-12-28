8e0bce2f991acd41c105981846e5b1908b90ed2a
yangdi0710 <phanlengocduy07102000@gmail.com>
2023-12-28 16:54:06 +0700

    28/12

---
+++ pages/orders.js
@@ -0,0 +1,49 @@
+import Layout from "@/components/Layout";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default function OrderPage() {
+  const [orders, setOrders] = useState([]);
+  useEffect(() => {
+    axios.get("/api/orders").then((response) => {
+      setOrders(response.data);
+    });
+  }, []);
+  return (
+    <Layout>
+      <h1>Order</h1>
+      <table className="basic">
+        <thead>
+          <tr>
+            <td>Date</td>
+            <td>Recipient</td>
+            <td>Products</td>
+          </tr>
+        </thead>
+        <tbody>
+          {orders.length > 0 &&
+            orders.map((order, index) => (
+              <tr key={index}>
+                <td>{order.createdAt}</td>
+                <td>
+                  {order.name} <br />
+                  {order.email} <br />
+                  {order.streetAddress}
+                  {order.city} &nbsp;
+                  {order.postalCode} &nbsp;
+                  {order.country}
+                </td>
+                <td>
+                  {order.line_items.map((l) => (
+                    <>
+                      {l.quantity} * {l.price_data?.product_data.name} <br/>
+                    </>
+                  ))}
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </Layout>
+  );
+}
